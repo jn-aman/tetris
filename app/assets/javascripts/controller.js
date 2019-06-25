61167f1099d322c705ab26371a18e317def3a88b
jn-aman <jn_aman@yahoo.com>
2019-06-25 15:55:54 +0530

    aman

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -72,6 +72,22 @@
    };
  
  })(jQuery);
+
+
+$("canvas").touchwipe({
+     wipeLeft: function() { alert("left"); },
+     wipeRight: function() { alert("right"); },
+     wipeUp: function() { alert("up"); },
+     wipeDown: function() { alert("down"); },
+     min_move_x: 20,
+     min_move_y: 20,
+     preventDefaultEvents: true
+});
+
+
+
+
+
 document.body.onkeydown = function( e ) {
     var keys = {
         37: 'left',
